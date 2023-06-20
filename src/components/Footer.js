5ecf3dc53b69772d90068636dd44b09c5e8ddf96
syntaxland <chibuzo.okenwa@email.com>
2023-06-20 09:57:01 +0100

    updated src+

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,96 @@
+// import React from "react";
+// import { Container, Row, Col } from "react-bootstrap";
+
+// function Footer() {
+//   return (
+//     <footer>
+//       <Container>
+//         <Row>
+//           <Col className='text-center py-3'>@Copywrite McdofGlobal - Designed by SoftGlobal</Col>
+//         </Row>
+//       </Container>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
+
+// import React from "react";
+// import { Container, Row, Col } from "react-bootstrap";
+
+// function Footer() {
+//   return (
+//     <footer className="bg-dark text-light">
+//       <Container>
+//         <Row>
+//           <Col className="text-center py-3">
+//             &copy; McdofGlobal - Designed by SoftGlobal
+//           </Col>
+//         </Row>
+//       </Container>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
+
+// import React from "react";
+// import { Container } from "react-bootstrap";
+
+// function Footer() {
+//   return (
+//     <footer className="bg-dark text-light">
+//       <Container fluid className="py-3">
+//         <p className="text-center m-0">
+//           &copy; McdofGlobal - Designed by SoftGlobal
+//         </p>
+//       </Container>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
+
+
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
   return (
-    <footer>
-      <Container>
+    <footer className="bg-dark text-light">
+      <Container fluid>
+        <Row className="py-3">
+          <Col className="text-center">
+            <h5>Company</h5>
+            <ul className="list-unstyled">
+              <li>About Us</li>
+              <li>Contact Us</li>
+              <li>Terms of Service</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </Col>
+          <Col className="text-center">
+            <h5>Categories</h5>
+            <ul className="list-unstyled">
+              <li>Electronics</li>
+              <li>Clothing</li>
+              <li>Home &amp; Garden</li>
+              <li>Books</li>
+            </ul>
+          </Col>
+          <Col className="text-center">
+            <h5>Support</h5>
+            <ul className="list-unstyled">
+              <li>FAQs</li>
+              <li>Shipping &amp; Returns</li>
+              <li>Customer Service</li>
+            </ul>
+          </Col>
+        </Row>
         <Row>
-          <Col className='text-center py-3'>@copy McdofGlobal - Designed by SoftGlobal</Col>
+          <Col className="text-center py-3">
+            &copy; McdofGlobal - Designed by SoftGlobal
+          </Col>
         </Row>
       </Container>
     </footer>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,71 @@
+import React from "react";
+import { Navbar, Nav, Container, Button, NavDropdown, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { logout } from "../actions/userActions";
+import { useDispatch, useSelector } from "react-redux";
+
+function Header() {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" sticky="top">
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/">
+            <i className="fas fa-home"></i> Mcdof Global Store
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarCollapse" />
+          <Navbar.Collapse id="navbarCollapse">
+            <Nav className="mr-auto">
+              <Form className="d-flex flex-grow-1">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="mr-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Nav>
+            <Nav>
+              <NavDropdown title="All" id="navbarScrollingDropdown">
+                <NavDropdown.Item as={Link} to="/login">Sign in</NavDropdown.Item>
+                {/* <NavDropdown.Item as={Link} to="/logout">Logout</NavDropdown.Item> */}
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to="/">Favourite</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link as={Link} to="/sell">Sell</Nav.Link>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <Nav.Link as={Link} to="/login">
+                  <i className="fas fa-user"></i> Hello, sign in
+                </Nav.Link>
+              )}
+              <Nav.Link as={Link} to="/cart">
+                <i className="fas fa-shopping-cart"></i> Cart
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
+
 // import React from "react";
-// import { Navbar, Nav, Container, Row, NavDropdown } from "react-bootstrap";
-// import { LinkContainer } from "react-router-bootstrap";
+// import { Navbar, Nav, Container, Button, NavDropdown, Form } from "react-bootstrap";
+// import { Link } from "react-router-dom";
 // import { logout } from "../actions/userActions";
 // import { useDispatch, useSelector } from "react-redux";
 
@@ -13,146 +78,275 @@
 //   };
 
 //   return (
-//     <div>
-//       <Navbar bg="dark" variant="dark">
-//         <Container>
-//           <LinkContainer to="/" LinkContainer>
-//             <Navbar.Brand>A.R.K</Navbar.Brand>
-//           </LinkContainer>
-
-//           <Navbar.Toggle aria-controls="navbarScroll" />
-//           <Navbar.Collapse id="navbarScroll">
-//             <Nav
-//               className="mr-auto my-2 my-lg-0"
-//               style={{ maxHeight: "100px" }}
-//               navbarScroll
-//             >
-//               <LinkContainer to="/">
-//                 <Nav.Link>
-//                   <i className="fas fa-home"></i> Home
-//                 </Nav.Link>
-//               </LinkContainer>
-
-//               <LinkContainer to="/cart">
-//                 <Nav.Link>
-//                   <i className="fas fa-shopping-cart"></i> Cart
-//                 </Nav.Link>
-//               </LinkContainer>
+//     <header>
+//       <Navbar bg="dark" variant="dark" expand="md" sticky="top">
+//         <Container fluid>
+//           <Navbar.Brand as={Link} to="/">
+//             <i className="fas fa-home"></i> Mcdof Global Store
+//           </Navbar.Brand>
+//           <Navbar.Toggle aria-controls="navbarCollapse" />
+//           <Navbar.Collapse id="navbarCollapse">
+//             <Nav className="mr-auto">
+//               <Form className="d-flex flex-grow-1">
+//                 <Form.Control
+//                   type="search"
+//                   placeholder="Search"
+//                   className="me-2"
+//                   aria-label="Search"
+//                 />
+//                 <Button variant="outline-success">Search</Button>
+//               </Form>
+//             </Nav>
+//             <Nav>
+//               <NavDropdown title="All" id="navbarScrollingDropdown">
+//                 <NavDropdown.Item as={Link} to="/login">Sign in</NavDropdown.Item>
+//                 <NavDropdown.Item as={Link} to="/logout">Logout</NavDropdown.Item>
+//                 <NavDropdown.Divider />
+//                 <NavDropdown.Item as={Link} to="/">Favourite</NavDropdown.Item>
+//               </NavDropdown>
+//               <Nav.Link as={Link} to="/sell">Sell</Nav.Link>
 //               {userInfo ? (
 //                 <NavDropdown title={userInfo.name} id="username">
-//                   <LinkContainer to="/profile">
-//                     <NavDropdown.Item>Profile</NavDropdown.Item>
-//                   </LinkContainer>
-
-//                   <NavDropdown.Item onClick={logoutHandler}>
-//                     Logout
-//                   </NavDropdown.Item>
+//                   <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+//                   <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
 //                 </NavDropdown>
 //               ) : (
-//                 <LinkContainer to="/login">
-//                   <Nav.Link>
-//                     <i className="fas fa-user"></i> Login
-//                   </Nav.Link>
-//                 </LinkContainer>
+//                 <Nav.Link as={Link} to="/login">
+//                   <i className="fas fa-user"></i> Hello, sign in
+//                 </Nav.Link>
 //               )}
+//               <Nav.Link as={Link} to="/cart">
+//                 <i className="fas fa-shopping-cart"></i> Cart
+//               </Nav.Link>
 //             </Nav>
 //           </Navbar.Collapse>
 //         </Container>
 //       </Navbar>
+//     </header>
+//   );
+// }
+
+// export default Header;
+
+// import React from "react";
+// import {
+//   Navbar,
+//   Nav,
+//   Container,
+//   Button,
+//   NavDropdown,
+//   Form,
+// } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import { logout } from "../actions/userActions";
+// import { useDispatch, useSelector } from "react-redux";
+// // import { LinkContainer } from "react-router-bootstrap";
+
+// function Header() {
+//   const userLogin = useSelector((state) => state.userLogin);
+//   const { userInfo } = userLogin;
+//   const dispatch = useDispatch();
+//   const logoutHandler = () => {
+//     dispatch(logout());
+//   };
+
+//   return (
+//     <div>
+//       <header>
+//         <Navbar bg="dark" variant="dark">
+//           <Container fluid>
+//             <Navbar.Brand as={Link} to="/">
+//               <i className="fas fa-home"></i>
+//               Mcdof Global Store
+//             </Navbar.Brand>
+
+//             <Navbar.Toggle aria-controls="navbarScroll" />
+//             <Navbar.Collapse id="navbarScroll">
+//               <Nav
+//                 className="me-auto my-2 my-lg-0"
+//                 style={{ maxHeight: "100px" }}
+//                 navbarScroll
+//               >
+//                 <Form className="d-flex">
+//                   <Form.Control
+//                     type="search"
+//                     placeholder="Search"
+//                     className="me-2"
+//                     aria-label="Search"
+//                   />
+//                   <Button variant="outline-success">Search</Button>
+//                 </Form>
+
+//                 <NavDropdown title="All" id="navbarScrollingDropdown">
+//                   <NavDropdown.Item as={Link} to="/login">Sign in
+//                   </NavDropdown.Item>
+//                   <NavDropdown.Item as={Link} to="/logout">Logout
+//                   </NavDropdown.Item>
+//                   <NavDropdown.Divider />
+//                   <NavDropdown.Item as={Link} to="/">
+//                     Favourite
+//                   </NavDropdown.Item>
+//                 </NavDropdown>
+
+//                 <Nav.Link as={Link} to="/sell">
+//                   Sell
+//                 </Nav.Link>
+
+//                 {userInfo ? (
+//                   <NavDropdown title={userInfo.name} id="username">
+//                     <Nav.Link as={Link} to="/profile">
+//                     profile
+//                     </Nav.Link>
+
+//                     <NavDropdown.Item onClick={logoutHandler}>Logout
+//                     </NavDropdown.Item>
+//                   </NavDropdown>
+//                 ) : (
+//                   <Nav.Link as={Link} to="/login">
+//                     <i className="fas fa-user"></i>Hello, sign in
+//                   </Nav.Link>
+//                 )}
+//                 <Nav.Link as={Link} to="/cart">
+//                   <i className="fas fa-shopping-cart"></i>Cart
+//                 </Nav.Link>
+//               </Nav>
+//             </Navbar.Collapse>
+//           </Container>
+//         </Navbar>
+//       </header>
 //     </div>
 //   );
 // }
 
 // export default Header;
 
-import React from "react";
-import {
-  Navbar,
-  Nav,
-  Container,
-  Button,
-  NavDropdown,
-  Form,
-} from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { logout } from "../actions/userActions";
-import { useDispatch, useSelector } from "react-redux";
-// import { LinkContainer } from "react-router-bootstrap";
 
-function Header() {
-  const userLogin = useSelector((state) => state.userLogin);
-  const { userInfo } = userLogin;
-  const dispatch = useDispatch();
-  const logoutHandler = () => {
-    dispatch(logout());
-  };
 
-  return (
-    <div>
-      <header>
-        <Navbar bg="dark" variant="dark">
-          <Container fluid>
-            <Navbar.Brand as={Link} to="/">
-              <i className="fas fa-home"></i>
-              Mcdof Global Store
-            </Navbar.Brand>
+// import React from "react";
+// import { Navbar, Nav, Container, Button, NavDropdown, Form } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import { logout } from "../actions/userActions";
+// import { useDispatch, useSelector } from "react-redux";
 
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-              <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: "100px" }}
-                navbarScroll
-              >
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
+// function Header() {
+//   const userLogin = useSelector((state) => state.userLogin);
+//   const { userInfo } = userLogin;
+//   const dispatch = useDispatch();
+//   const logoutHandler = () => {
+//     dispatch(logout());
+//   };
 
-                <NavDropdown title="All" id="navbarScrollingDropdown">
-                  <NavDropdown.Item as={Link} to="/login">Sign in
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/logout">Logout
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item as={Link} to="/">
-                    Favourite
-                  </NavDropdown.Item>
-                </NavDropdown>
+//   return (
+//     <header>
+//       <Navbar bg="dark" variant="dark" expand="md" fixed="top">
+//         <Container fluid>
+//           <Navbar.Brand as={Link} to="/">
+//             <i className="fas fa-home"></i> Mcdof Global Store
+//           </Navbar.Brand>
+//           <Navbar.Toggle aria-controls="navbarCollapse" />
+//           <Navbar.Collapse id="navbarCollapse">
+//             <Nav className="mr-auto">
+//               <Form className="d-flex flex-grow-1">
+//                 <Form.Control
+//                   type="search"
+//                   placeholder="Search"
+//                   className="me-2"
+//                   aria-label="Search"
+//                 />
+//                 <Button variant="outline-success">Search</Button>
+//               </Form>
+//             </Nav>
+//             <Nav>
+//               <NavDropdown title="All" id="navbarScrollingDropdown">
+//                 <NavDropdown.Item as={Link} to="/login">Sign in</NavDropdown.Item>
+//                 <NavDropdown.Item as={Link} to="/logout">Logout</NavDropdown.Item>
+//                 <NavDropdown.Divider />
+//                 <NavDropdown.Item as={Link} to="/">Favourite</NavDropdown.Item>
+//               </NavDropdown>
+//               <Nav.Link as={Link} to="/sell">Sell</Nav.Link>
+//               {userInfo ? (
+//                 <NavDropdown title={userInfo.name} id="username">
+//                   <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+//                   <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+//                 </NavDropdown>
+//               ) : (
+//                 <Nav.Link as={Link} to="/login">
+//                   <i className="fas fa-user"></i> Hello, sign in
+//                 </Nav.Link>
+//               )}
+//               <Nav.Link as={Link} to="/cart">
+//                 <i className="fas fa-shopping-cart"></i> Cart
+//               </Nav.Link>
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Container>
+//       </Navbar>
+//     </header>
+//   );
+// }
 
-                <Nav.Link as={Link} to="/sell">
-                  Sell
-                </Nav.Link>
+// export default Header;
 
-                {userInfo ? (
-                  <NavDropdown title={userInfo.name} id="username">
-                    <Nav.Link as={Link} to="/profile">
-                    profile
-                    </Nav.Link>
 
-                    <NavDropdown.Item onClick={logoutHandler}>Logout
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                ) : (
-                  <Nav.Link as={Link} to="/login">
-                    <i className="fas fa-user"></i>Hello, sign in
-                  </Nav.Link>
-                )}
-                <Nav.Link as={Link} to="/cart">
-                  <i className="fas fa-shopping-cart"></i>Cart
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    </div>
-  );
-}
+// import React from "react";
+// import { Navbar, Nav, Container, Button, NavDropdown, Form } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import { logout } from "../actions/userActions";
+// import { useDispatch, useSelector } from "react-redux";
 
-export default Header;
+// function Header() {
+//   const userLogin = useSelector((state) => state.userLogin);
+//   const { userInfo } = userLogin;
+//   const dispatch = useDispatch();
+//   const logoutHandler = () => {
+//     dispatch(logout());
+//   };
+
+//   return (
+//     <header>
+//       <Navbar bg="dark" variant="dark" expand="md" fixed="top">
+//         <Container fluid>
+//           <Navbar.Brand as={Link} to="/">
+//             <i className="fas fa-home"></i> Mcdof Global Store
+//           </Navbar.Brand>
+//           <Navbar.Toggle aria-controls="navbarCollapse" />
+//           <Navbar.Collapse id="navbarCollapse">
+//             <Nav className="ml-auto">
+//               <Form className="d-flex flex-grow-1">
+//                 <Form.Control
+//                   type="search"
+//                   placeholder="Search"
+//                   className="me-2"
+//                   aria-label="Search"
+//                 />
+//                 <Button variant="outline-success">Search</Button>
+//               </Form>
+//               <NavDropdown title="All" id="navbarScrollingDropdown">
+//                 <NavDropdown.Item as={Link} to="/login">Sign in</NavDropdown.Item>
+//                 <NavDropdown.Item as={Link} to="/logout">Logout</NavDropdown.Item>
+//                 <NavDropdown.Divider />
+//                 <NavDropdown.Item as={Link} to="/">Favourite</NavDropdown.Item>
+//               </NavDropdown>
+//               <Nav.Link as={Link} to="/sell">Sell</Nav.Link>
+//               {userInfo ? (
+//                 <NavDropdown title={userInfo.name} id="username">
+//                   <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+//                   <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+//                 </NavDropdown>
+//               ) : (
+//                 <Nav.Link as={Link} to="/login">
+//                   <i className="fas fa-user"></i> Hello, sign in
+//                 </Nav.Link>
+//               )}
+//               <Nav.Link as={Link} to="/cart">
+//                 <i className="fas fa-shopping-cart"></i> Cart
+//               </Nav.Link>
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Container>
+//       </Navbar>
+//     </header>
+//   );
+// }
+
+// export default Header;
